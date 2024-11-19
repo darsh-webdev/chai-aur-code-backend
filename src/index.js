d35aae88660bb9e3143afb39f348c8c350725e2b
Darshan Rajput <rajputdarshanashok@gmail.com>
2024-11-19 23:30:36 +0530

    Add custom api response and error handling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: " + error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("DB Connection failed!", err);
+  });
 
 // import express from "express";
 // const app = express();
